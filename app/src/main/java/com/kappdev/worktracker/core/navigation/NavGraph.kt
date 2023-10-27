cae6c412a3f86d12d13154633c81084dcfc94758
Khotych Mykola <kola13r@gmail.com>
2023-10-27 16:19:47 +0300

    Updated navigations between screens

--- app/src/main/java/com/kappdev/worktracker/core/navigation/NavGraph.kt
+++ app/src/main/java/com/kappdev/worktracker/core/navigation/NavGraph.kt
@@ -1,10 +1,10 @@
 package com.kappdev.worktracker.core.navigation
 
 import androidx.compose.animation.*
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.navArgument
@@ -37,26 +37,48 @@ fun SetupNavGraph(
         navController = navController,
         startDestination = Screen.SplashScreen.route
     ) {
-        composable(Screen.Main.route) {
+        composable(
+            Screen.Main.route,
+            enterTransition = {
+                when {
+                    navigatesFrom(Screen.AddEditActivity) -> slideInRight()
+                    navigatesFrom(Screen.ActivityReview) -> slideInRight()
+                    navigatesFrom(Screen.Settings) -> cornerScaleIn(Corner.BOTTOM_LEFT)
+                    else -> null
+                }
+            },
+            exitTransition = {
+                when {
+                    navigatesToward(Screen.AddEditActivity) -> slideOutLeft()
+                    navigatesToward(Screen.ActivityReview) -> slideOutLeft()
+                    navigatesToward(Screen.Settings) -> cornerScaleOut(Corner.BOTTOM_LEFT)
+                    else -> null
+                }
+            }
+        ) {
             MainScreen(navController, stopwatchService, countdownService)
         }
 
-        composable(Screen.WorkStatistic.route) {
+        composable(
+            Screen.WorkStatistic.route,
+            enterTransition = { screenFadeIn() },
+            exitTransition = { screenFadeOut() }
+        ) {
             WorkStatisticScreen(navController)
         }
 
-        composable(Screen.Settings.route) {
+        composable(
+            Screen.Settings.route,
+            enterTransition = { cornerScaleIn(Corner.TOP_RIGHT) },
+            exitTransition = { cornerScaleOut(Corner.TOP_RIGHT) }
+        ) {
             SettingsScreen(navController)
         }
 
         composable(
             Screen.SplashScreen.route,
-            enterTransition = {
-                fadeIn(tween(400))
-            },
-            exitTransition = {
-                fadeOut(tween(400))
-            }
+            enterTransition = { screenFadeIn() },
+            exitTransition = { screenFadeOut() }
         ) {
             SplashScreen {
                 navController.navigate(startDestination)
@@ -65,48 +87,16 @@ fun SetupNavGraph(
 
         composable(
             Screen.StopwatchTimer.route,
-            enterTransition = {
-                slideInVertically(
-                    initialOffsetY = { it },
-                    animationSpec = spring(stiffness = Spring.StiffnessLow)
-                ) + fadeIn(
-                    animationSpec = spring(stiffness = Spring.StiffnessLow),
-                    initialAlpha = 0.5f
-                )
-            },
-            exitTransition = {
-                slideOutVertically(
-                    targetOffsetY = { it },
-                    animationSpec = spring(stiffness = Spring.StiffnessLow)
-                ) + fadeOut(
-                    animationSpec = spring(stiffness = Spring.StiffnessLow),
-                    targetAlpha = 0.5f
-                )
-            }
+            enterTransition = { slideInUp() },
+            exitTransition = { slideOutDown() }
         ) {
             StopwatchTimerScreen(navController, stopwatchService, stopwatchController)
         }
 
         composable(
             Screen.CountdownTimer.route,
-            enterTransition = {
-                slideInVertically(
-                    initialOffsetY = { it },
-                    animationSpec = spring(stiffness = Spring.StiffnessLow)
-                ) + fadeIn(
-                    animationSpec = spring(stiffness = Spring.StiffnessLow),
-                    initialAlpha = 0.5f
-                )
-            },
-            exitTransition = {
-                slideOutVertically(
-                    targetOffsetY = { it },
-                    animationSpec = spring(stiffness = Spring.StiffnessLow)
-                ) + fadeOut(
-                    animationSpec = spring(stiffness = Spring.StiffnessLow),
-                    targetAlpha = 0.5f
-                )
-            }
+            enterTransition = { slideInUp() },
+            exitTransition = { slideOutDown() }
         ) {
             CountdownTimerScreen(navController, countdownService, countdownController)
         }
@@ -120,16 +110,13 @@ fun SetupNavGraph(
                 }
             ),
             enterTransition = {
-                slideInHorizontally(
-                    initialOffsetX = { it },
-                    animationSpec = spring(stiffness = Spring.StiffnessLow)
-                )
+                slideInLeft()
             },
             exitTransition = {
-                slideOutHorizontally(
-                    targetOffsetX = { it },
-                    animationSpec = spring(stiffness = Spring.StiffnessLow)
-                )
+                when {
+                    navigatesToward(Screen.AddEditActivity) -> slideOutLeft()
+                    else -> slideOutRight()
+                }
             }
         ) {
             val activityId = it.arguments?.getLong("activityId")
@@ -145,10 +132,84 @@ fun SetupNavGraph(
                     type = NavType.LongType
                     defaultValue = 0
                 }
-            )
+            ),
+            enterTransition = { slideInLeft() },
+            exitTransition = { slideOutRight() }
         ) {
             val activityId = it.arguments?.getLong("activityId")
             AddEditActivityScreen(navController, activityId)
         }
     }
 }
+
+private fun screenFadeIn() = fadeIn(animationSpec = tween(ANIM_DURATION))
+private fun screenFadeOut() = fadeOut(animationSpec = tween(ANIM_DURATION))
+
+private fun slideInLeft() = slideInHorizontally(animationSpec = tween(ANIM_DURATION)) { it }
+private fun slideInRight() = slideInHorizontally(animationSpec = tween(ANIM_DURATION)) { -it }
+
+private fun slideOutLeft() = slideOutHorizontally(animationSpec = tween(ANIM_DURATION)) { -it }
+private fun slideOutRight() = slideOutHorizontally(animationSpec = tween(ANIM_DURATION)) { it }
+
+
+private enum class Corner { TOP_LEFT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_RIGHT }
+
+private fun Corner.getOrigin() = when (this) {
+    Corner.TOP_LEFT -> TransformOrigin(0f, 0f)
+    Corner.TOP_RIGHT -> TransformOrigin(1f, 0f)
+    Corner.BOTTOM_LEFT -> TransformOrigin(0f, 1f)
+    Corner.BOTTOM_RIGHT -> TransformOrigin(1f, 1f)
+}
+
+@OptIn(ExperimentalAnimationApi::class)
+private fun cornerScaleIn(corner: Corner) = scaleIn(
+    animationSpec = tween(ANIM_DURATION),
+    transformOrigin = corner.getOrigin()
+)
+
+@OptIn(ExperimentalAnimationApi::class)
+private fun cornerScaleOut(corner: Corner) = scaleOut(
+    animationSpec = tween(ANIM_DURATION),
+    transformOrigin = corner.getOrigin()
+)
+
+
+@OptIn(ExperimentalAnimationApi::class)
+private fun AnimatedContentScope<NavBackStackEntry>.slideInUp(): EnterTransition {
+    return slideIntoContainer(
+        towards = AnimatedContentScope.SlideDirection.Up,
+        animationSpec = tween(ANIM_DURATION)
+    )
+}
+
+@OptIn(ExperimentalAnimationApi::class)
+private fun AnimatedContentScope<NavBackStackEntry>.slideOutDown(): ExitTransition {
+    return slideOutOfContainer(
+        towards = AnimatedContentScope.SlideDirection.Down,
+        animationSpec = tween(ANIM_DURATION)
+    )
+}
+
+
+
+@OptIn(ExperimentalAnimationApi::class)
+private fun AnimatedContentScope<NavBackStackEntry>.navigatesToward(screen: Screen): Boolean {
+    return getTargetRoute()?.contains(screen.route) ?: false
+}
+
+@OptIn(ExperimentalAnimationApi::class)
+private fun AnimatedContentScope<NavBackStackEntry>.getTargetRoute(): String? {
+    return this.targetState.destination.route
+}
+
+@OptIn(ExperimentalAnimationApi::class)
+private fun AnimatedContentScope<NavBackStackEntry>.navigatesFrom(screen: Screen): Boolean {
+    return getInitialRoute()?.contains(screen.route) ?: false
+}
+
+@OptIn(ExperimentalAnimationApi::class)
+private fun AnimatedContentScope<NavBackStackEntry>.getInitialRoute(): String? {
+    return this.initialState.destination.route
+}
+
+private const val ANIM_DURATION = 700
