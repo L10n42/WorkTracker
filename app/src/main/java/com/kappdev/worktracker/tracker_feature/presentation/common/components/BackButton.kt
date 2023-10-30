b7c47df08352ce539f9424a79cf56bffe9755780
Khotych Mykola <kola13r@gmail.com>
2023-10-30 10:22:57 +0200

    Colors added to WorkStatisticScreen

--- app/src/main/java/com/kappdev/worktracker/tracker_feature/presentation/common/components/BackButton.kt
+++ app/src/main/java/com/kappdev/worktracker/tracker_feature/presentation/common/components/BackButton.kt
@@ -6,15 +6,18 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 
 @Composable
 fun BackButton(
     icon: ImageVector = Icons.Default.ArrowBack,
+    modifier: Modifier = Modifier,
     onClick: () -> Unit
 ) {
     IconButton(
-        onClick = onClick
+        onClick = onClick,
+        modifier = modifier
     ) {
         Icon(
             imageVector = icon,
